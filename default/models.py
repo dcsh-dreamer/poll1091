aeed7d7ce8f28b2f8817951bfaf9e0b26c2971f6
Dino Wei <dreamre@dcsh.tp.edu.tw>
2020-10-29 10:29:46 +0800

    定義資料模型

--- default/models.py
+++ default/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):    
+    subject = models.CharField(max_length=200)
+    description = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ") " + self.subject
+
+class Option(models.Model):
+    poll_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id) + ")" + self.title + "@" + str(self.poll_id)
